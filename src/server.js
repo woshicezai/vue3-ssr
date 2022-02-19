43b82917dd27cb3d76b695e50fa25317856c0a7d
zhouce <zhouce@hualala.com>
2022-02-19 20:15:19 +0800

    feat: finish server css file add

--- src/server.js
+++ src/server.js
@@ -5,9 +5,13 @@ const { renderToString } = require("@vue/server-renderer");
 const manifest = require("../dist/ssr-manifest.json");
 
 const appPath = path.join(__dirname, "../dist", manifest["app.js"]);
+const cssPath = manifest["app.css"];
+
 const App = require(appPath).default;
 
 const server = express();
+
+server.use("/css", express.static(path.join(__dirname, "../dist", "css")));
 
 server.get("*", async (req, res) => {
   const app = createSSRApp(App);
@@ -15,6 +19,7 @@ server.get("*", async (req, res) => {
   const html = `
         <html>
             <head></head>
+            <link rel="stylesheet" href='${cssPath}'></link>
             <body>
              ${appContent}
             </body>
